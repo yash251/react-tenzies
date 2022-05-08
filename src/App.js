650aeea647325f9e2426b2aefbee2c301ee6d821
Yash Jagtap <jagtapyash2512@gmail.com>
2022-05-08 19:07:59 +0530

    display grid die

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './style.css';
+import Die from "./Die"
 
 function App() {
   return (
-    <div>
-      <main>Something</main>
-    </div>
+    <main>
+        <div className='dice-container'>
+            <Die value="1" />
+            <Die value="2" />
+            <Die value="3" />
+            <Die value="4" />
+            <Die value="5" />
+            <Die value="6" />
+            <Die value="1" />
+            <Die value="2" />
+            <Die value="3" />
+            <Die value="4" />
+        </div>
+      </main>
   );
 }
 
